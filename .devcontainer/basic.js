5c7a7877f509f1ef466ab6112934e8dafd73e889
Caspar Owner <casparowner@gmail.com>
2026-07-25 04:36:47 +0000

    done

--- .devcontainer/basic.js
+++ .devcontainer/basic.js
@@ -1,9 +1,2 @@
-const accountId = 122443
-let accountEmail = "hello@com"
-var accountPassword = "12234"
-accountCity = "sini"
-let accountstate;
-
-console.log(accountId);
-
-console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+let name = "alok";
+console.log(name)
